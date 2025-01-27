139f2b551d4d1983b855d4f5a21ea89f4905aed5
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2025-01-27 17:27:35 +0100

    🔖 / fin td2

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,10 @@
 <script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import WelcomeItem from './WelcomeItem.vue';
+import DocumentationIcon from './icons/IconDocumentation.vue';
+import ToolingIcon from './icons/IconTooling.vue';
+import EcosystemIcon from './icons/IconEcosystem.vue';
+import CommunityIcon from './icons/IconCommunity.vue';
+import SupportIcon from './icons/IconSupport.vue';
 </script>
 
 <template>
@@ -12,10 +12,16 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Create Vue project with Vite</template>
+    <template #heading>
+      Create Vue project with Vite
+    </template>
 
     Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+      rel="noopener"
+    >official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
 
@@ -23,7 +29,9 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Create Github repository</template>
+    <template #heading>
+      Create Github repository
+    </template>
     Go to <a href="https://github.com/new">Github</a> and create a new repository. Make sure Public
     is selected if you don't have a premium account. Otherwise, you won't be able to host your app
     using Github Page.
@@ -33,18 +41,30 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <EcosystemIcon />
     </template>
-    <template #heading>Write test</template>
+    <template #heading>
+      Write test
+    </template>
 
     To test your components and web pages, check out
-    <a href="https://www.vitest.dev/" target="_blank" rel="noopener">Vitest</a> and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
+    <a
+      href="https://www.vitest.dev/"
+      target="_blank"
+      rel="noopener"
+    >Vitest</a> and
+    <a
+      href="https://www.cypress.io/"
+      target="_blank"
+      rel="noopener"
+    >Cypress</a>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Create workflow</template>
+    <template #heading>
+      Create workflow
+    </template>
     Go to "Actions Settings", select Read and write permissions and hit Save. Create a new file:
     <code>.github/workflows/deploy.yml</code> and add your deploy script. You will need to :
     <ul>
@@ -64,7 +84,9 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <SupportIcon />
     </template>
-    <template #heading>Enjoy Github page</template>
+    <template #heading>
+      Enjoy Github page
+    </template>
     Go to Pages Settings, set Source to gh-pages, and hit Save. Now you just have to push on the
     main branch of your repository and you're good.
   </WelcomeItem>
--- src/stores/__tests__/counter.spec.js
+++ src/stores/__tests__/counter.spec.js
@@ -1,17 +1,17 @@
-import { setActivePinia, createPinia } from 'pinia'
-import { describe, it, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia';
+import { describe, it, beforeEach } from 'vitest';
 
 describe('Counter Store', () => {
   beforeEach(() => {
     // creates a fresh pinia and makes it active
     // so it's automatically picked up by any useStore() call
     // without having to pass it to it: `useStore(pinia)`
-    setActivePinia(createPinia())
-  })
+    setActivePinia(createPinia());
+  });
 
   it('increments', () => {
-  })
+  });
 
   it('decrements', () => {
-  })
-})
+  });
+});
